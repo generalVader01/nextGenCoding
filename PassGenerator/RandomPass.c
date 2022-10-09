4778e2a1625e4ff66adcc9aae27e26f0c9a23783
generalVader01 <84896577+generalVader01@users.noreply.github.com>
2022-10-08 18:01:14 -0700

    Add files via upload
    Big update. Added options to enable spaces, symbols, numbers, uppercase, and undercase in the charset. Limits is enabled.

--- PassGenerator/RandomPass.c
+++ PassGenerator/RandomPass.c
@@ -3,7 +3,13 @@
 #include <time.h>
 #include <limits.h>
 
-#define Gen_Array_Length 70 
+#define TRUE 1
+#define FALSE 0
+#define UPPERCASE TRUE
+#define UNDERCASE TRUE
+#define NUMBERS TRUE
+#define SYMBOLS TRUE
+#define SPACES TRUE
 
 // ***** Summary ***** 
 // Generates a psudo random string of characters. 
@@ -15,8 +21,9 @@
  * 1.01: Added the space character to the generate list
  * 1.02: Fixed return values. Errors are now returning negative 1. 0 is returned only for success
  * 1.03: Removed Magic Number for the Character Source List for Generation. It is now defined as a variable
- * 1.04: Changed input from signed int to unsigned int
+ * 1.04: Changed input from signed int to unsigned int.
  * 1.05: included limits library. Now checking to make sure that specified input does not exceed limits of data type
+ * 1.06: Big update. Added options to enable spaces, symbols, numbers, uppercase, and undercase in the charset. 
 */
 
 int main(int argc, char *argv[]) {
@@ -26,11 +33,12 @@ int main(int argc, char *argv[]) {
 		printf("Usage: ./RandomPass $NumberOfPasswords $PasswordLength\n");
 		return -1;
 	}
-
-	char randomLetters[Gen_Array_Length];
-	unsigned int randomWordLength = atoi(argv[2]);
+	
+	// note: randomLetter array is declared as size 95 because that is the max possible size with all character types enabled
+	unsigned char genLength = 0;
+	unsigned char randomLetters[95];
 	unsigned int passGenerations = atoi(argv[1]);
-
+	unsigned int randomWordLength = atoi(argv[2]);
 
 	if(randomWordLength <= 0 || passGenerations <= 0)
 	{
@@ -45,24 +53,65 @@ int main(int argc, char *argv[]) {
 	        printf("Please specify a input value less than %u\n", UINT_MAX);
 		return -1;
 	}	
-
-/* Some symbols + letters A-Z in the ASCII table */
-
-	for(int i = 0; i < 43; i++)
+	
+	// Add a Space if enabled 	
+	if(SPACES == TRUE)
 	{
-		randomLetters[i] = (char) (48 + i);
+		randomLetters[genLength] = ' ';
+		genLength++;
 	}
 
-/* Builds letters a-z (undercase) in the ASCII table */
-
-	for(int i = 43; i < 69; i++)
+	// Adds symbols if enabled
+	if(SYMBOLS == TRUE)
 	{
-		randomLetters[i] = (char) (54 + i);
+		for(int i = 33; i <= 47; i++, genLength++)
+		{
+			randomLetters[genLength] = (char) i;
+	        }
+
+		for(int i = 58; i <= 64; i++, genLength++)
+		{
+			randomLetters[genLength] = (char) i;
+		}
+		
+		for(int i = 91; i <= 96; i++, genLength++)
+		{
+			randomLetters[genLength] = (char) i;
+		}
+
+		for(int i = 123; i <= 126; i++, genLength++)
+		{
+			randomLetters[genLength] = (char) i;
+		}
+ 	}
+	
+
+	// Adds numbers if enabled
+	if(NUMBERS == TRUE)
+        {
+                for(int i = 48; i <= 57; i++, genLength++)
+                {
+                	randomLetters[genLength] = (char) i;
+                }
+        }
+
+	// Adds uppercase if enabled	
+	if(UPPERCASE == TRUE)
+        {
+                for(int i = 65; i <= 90; i++, genLength++)
+                {
+                        randomLetters[genLength] = (char) i;
+                }
+        }
+	
+	// Add undercase if enabled
+	if(UNDERCASE == TRUE)
+	{	
+		for(int i = 97; i <= 122; i++, genLength++)
+		{
+			randomLetters[genLength] = (char) i;
+		}
 	}
-/* Add Space Character */
-
-randomLetters[Gen_Array_Length - 1] = (char) 32;
-
 
 	time_t t;
 	srand((unsigned) time(&t));
@@ -73,7 +122,7 @@ randomLetters[Gen_Array_Length - 1] = (char) 32;
 	{
 		for(int i = 0; i < randomWordLength; i++)
 		{
-			printf("%c", randomLetters[rand() % Gen_Array_Length]);
+			printf("%c", randomLetters[rand() % genLength]);
 					
 		}
 		printf("\n");
